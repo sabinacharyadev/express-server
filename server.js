3622d4dfb4e6465a395e62f68ba2c0e1574e1b7b
sabinbabu <sabinacharya444@gmail.com>
2025-03-24 12:58:53 +1100

    json api path added

--- server.js
+++ server.js
@@ -6,8 +6,14 @@ const app = express();
 // Define PORT to run our server
 const PORT = 3000;
 
+// Send "Hello express server" to client requesting on "/" path
 app.get("/", (req, res) => {
   res.send("Hello express server");
+});
+
+// Send a json object { data: "Hello express server" } to client requesting on "/json" path
+app.get("/json", (req, res) => {
+  res.json({ data: "Hello express server" });
 });
 
 // App will start listening for incoming requests on the specified PORT.
